4bc5cafc03f66392f8692dd3fa6c8dc247c5504a
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-28 20:25:13 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -20,5 +20,6 @@ end
   #  expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
   
   def age_appropriate_birthday(birthday_kids)
-    age=
+    appropriate_age=""
+    birthday_kids.each do |name,age|
     
