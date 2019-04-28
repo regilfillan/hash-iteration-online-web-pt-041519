c90d8163d3cb4000e7b58b4988de46456976dd85
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-28 20:24:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,16 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+#describe "#age_appropriate_birthday" do
+ # it "only prints the birthday greeting if the birthday kid 12 or younger" do
+  #  birthday_kids = {
+ #     "Timmy" => 9,
+ #     "Sarah" => 6,
+ #     "Amanda" => 27
+  #  }
+  #  expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
+  
+  def age_appropriate_birthday(birthday_kids)
+    age=
+    
